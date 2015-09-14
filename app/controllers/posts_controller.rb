03da9733e5a9e0e2e3c4f4f50ba015b9d5ce6914
suhaibh <suhaib.habibullah@gmail.com>
2015-09-14 03:37:02 -0400

    Add simple form

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,45 @@
+class PostsController < ApplicationController
+  
+  def new
+   @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to root_url
+    else
+      render 'new' 
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+  end
+
+  def show
+  end
+
+  def destroy
+
+  end
+
+  def index
+    @posts = Post.all
+  end
+
+  private
+    def set_post
+      @post = Post.find(params[:id])
+    end
+
+    def post_params
+      params.require(:post).permit(:title, :body)
+    end
+end
