0e738bd1dcc4de46e78c95e55541cbd12a527e88
suhaibh <suhaib.habibullah@gmail.com>
2015-11-09 00:07:13 -0500

    Add basic posts controller test

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,33 +1,23 @@
 require 'test_helper'
 
 class PostsControllerTest < ActionController::TestCase
+  
+  def setup
+    @post = posts(:post)
+  end
+
   test "should get new" do
     get :new
     assert_response :success
   end
 
-  test "should get create" do
-    get :create
-    assert_response :success
-  end
-
   test "should get edit" do
-    get :edit
-    assert_response :success
-  end
-
-  test "should get update" do
-    get :update
+    get :edit, id: @post
     assert_response :success
   end
 
   test "should get show" do
-    get :show
-    assert_response :success
-  end
-
-  test "should get destroy" do
-    get :destroy
+    get :show, id: @post
     assert_response :success
   end
 
