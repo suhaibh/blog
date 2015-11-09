df55e0a2721afaf72f56de253027a6ebcac90ef6
suhaibh <suhaib.habibullah@gmail.com>
2015-11-08 19:23:23 -0500

    Add titleize before save callback on title for posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,14 @@
 class Post < ActiveRecord::Base
+
+before_save :title_format
+
 validates :title, presence: true
 validates :body, presence: true, length: { maximum: 10000 }
 
+
+	private
+
+	def title_format
+      self.title = title.titleize
+    end
 end
