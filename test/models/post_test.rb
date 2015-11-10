f79a658d3897922fbf5eec02b93a8f0814376109
suhaibh <suhaib.habibullah@gmail.com>
2015-11-10 00:09:10 -0500

    Add post model test

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	def setup
+		@post = posts(:post)
+	end
+
+	test "post should be valid" do
+		assert @post.valid?
+	end
+
+	test "empty title should be invalid" do
+		@post.title = ""
+		assert @post.invalid?
+	end
+
+	test "empty body should be invalid" do
+		@post.body = ""
+		assert @post.invalid?
+	end
+
+	test "body should be less than 1000 characters" do
+		@post.body = "a" * 10001
+		assert @post.invalid?
+	end
+
 end
